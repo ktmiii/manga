e4096b21de38538b4826b250f4c669d4466970fa
EC2 Default User <ec2-user@ip-172-31-0-86.ap-northeast-1.compute.internal>
2023-05-19 16:12:34 +0000

    input-image

--- app/models/user.rb
+++ app/models/user.rb
@@ -23,15 +23,15 @@ class User < ApplicationRecord
     favorite = self.favorites.find_by(book_id: book.id)
     favorite.destroy if favorite
   end
-  
+
   def follow(user_id)
     relationships.create(followed_id: user_id)
   end
-  
+
   def unfollow(user_id)
     relationships.find_by(followed_id: user_id).destroy
   end
-  
+
   def following?(user)
     followings.include?(user)
   end
